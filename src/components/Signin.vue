17b5b0f2f17fba94adf1528ac628b9c1a2402d5b
sallythetechie <sllmusanga7@gmail.com>
2021-08-21 16:38:42 +0300

    Add diagonal divs

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,84 +1,267 @@
 <template>
-  <div class="background">
-    <div class="top"></div>
-    <div class="bottom"><p class="stripe-headline">Stripe</p>
-    <p class="stripe-signup">Don't have an Account?<span class="pur">Signup</span></p>
-    <p class="stripe-footer">&copy;<a
-            href="https://stripe.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Stripe
-          </a>. <a
-            href="https://stripe.com/contact"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Contact
-          </a>.<a
-            href="https://stripe.com/privacy"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Privacy&Terms
-          </a></p>
-    </div>
-
-    <v-row justify="center" align="center" class="txt">
-      <v-col cols="12" sm="12" md="12">
-        <v-card class="signup">
-          <v-col cols="12">
-            <h1 class="display-1 black--text font-weight-bold mb-2">
-              Sign in to your account
-            </h1>
-
-            <h2 class="text-left">Email</h2>
-            <v-text-field
-              dense
-              v-model="email"
-              :rules="[emailrules.required, emailrules.email]"
-              color="#B0D8F0"
-              outlined
-              placeholder="jane.doe@gmail.com"
-            ></v-text-field>
-
-            <h2 class="left-text">Password</h2>
-            <h2 class="right-text pur">Forgot Password?</h2>
-
-            <v-text-field
-              dense
-              v-model="password"
-              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-              :rules="[rules.required, rules.min]"
-              :type="show1 ? 'text' : 'password'"
-              name="input-10-1"
-              hint="At least 8 characters"
-              counter
-              @click:append="show1 = !show1"
-              color="#B0D8F0"
-              outlined
-              placeholder="Password"
-            ></v-text-field>
-            <v-icon color="#635CFF">mdi-checkbox-marked</v-icon
-            ><span>Stay signed in for a week</span>
-
-            <v-btn @click="checkForm" color="#635CFF" block class="btn">
-              Continue</v-btn
-            >
-            <div>
-              <p class="text-center pur">Use single sign-on(SSO) instead</p>
+  <v-app>
+    <v-content>
+      <v-container class="px-4 py-2" fluid>
+        <v-layout row>
+          <div class="background">
+            <div class="top">
+              <div class="box">
+                <div class="wrapper"></div>
+              </div>
+              <div class="box1">
+                <div class="wrapper1"></div>
+              </div>
+              <div class="box2">
+                <div class="wrapper2"></div>
+              </div>
             </div>
-          </v-col>
-        </v-card>
-      </v-col>
-    </v-row>
-  </div>
+            <div class="bottom">
+              <p class="stripe-headline">Stripe</p>
+              <p class="stripe-signup">
+                Don't have an Account?<span class="pur">Signup</span>
+              </p>
+              <p class="stripe-footer">
+                &copy;<a
+                  href="https://stripe.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Stripe </a
+                >.
+                <a
+                  href="https://stripe.com/contact"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Contact </a
+                >.<a
+                  href="https://stripe.com/privacy"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Privacy&Terms
+                </a>
+              </p>
+            </div>
+
+            <v-row justify="center" align="center" class="txt">
+              <v-col cols="12" sm="12" md="12" >
+                <v-card class="signup ">
+                  <v-col cols="12">
+                    <h1 class="display-1 black--text font-weight-bold mb-2">
+                      Sign in to your account
+                    </h1>
+
+                    <h2 class="text-left">Email</h2>
+                    <v-text-field
+                      dense
+                      v-model="email"
+                      :rules="[emailrules.required, emailrules.email]"
+                      color="#B0D8F0"
+                      outlined
+                      placeholder="jane.doe@gmail.com"
+                    ></v-text-field>
+
+                    <h2 class="left-text">Password</h2>
+                    <h2 class="right-text pur">Forgot Password?</h2>
+
+                    <v-text-field
+                      dense
+                      v-model="password"
+                      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                      :rules="[rules.required, rules.min]"
+                      :type="show1 ? 'text' : 'password'"
+                      name="input-10-1"
+                      hint="At least 8 characters"
+                      counter
+                      @click:append="show1 = !show1"
+                      color="#B0D8F0"
+                      outlined
+                      placeholder="Password"
+                    ></v-text-field>
+                    <v-icon color="#635CFF">mdi-checkbox-marked</v-icon
+                    ><span>Stay signed in for a week</span>
+
+                    <v-btn @click="checkForm" color="#635CFF" block class="btn">
+                      Continue</v-btn
+                    >
+                    <div>
+                      <p class="text-center pur">
+                        Use single sign-on(SSO) instead
+                      </p>
+                    </div>
+                  </v-col>
+                </v-card>
+              </v-col>
+            </v-row>
+          </div>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 <script>
 export default {
-  name: "Signin",
+  name: "Dashboard",
   data() {
     return {
+      tabs: null,
+      sidebarMenu: true,
+      toggleMini: false,
+      singleSelect: false,
+      selected: [],
+      headers: [
+        {
+          text: "AMOUNT",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        { text: "DESCRIPTION", value: "description" },
+        { text: "CUSTOMER", value: "customer" },
+        { text: "DATE", value: "date" },
+      ],
+      users: [
+        {
+          name: "US$352.22 ",
+          icon: "mdi-account",
+          description: "fe3d8216-58f9-4dc1-aaef-2f19463b5258",
+          customer: "hefovo87@ereyemind",
+          date: "7 Jul, 14:04",
+        },
+
+        {
+          name: "US$39.99",
+          description: "0d7b6ac7-dbda-4157-bae9-e64749e538f5",
+          customer: "johndoe@gmail.com",
+          date: "7 Jul, 14:01",
+        },
+        {
+          name: "US$39.99",
+          description: "eca84b25-0324-4f31-b9fb-d489048b77ae",
+          customer: "jfamosinhoaraca@iseo...",
+          date: "7 Jul, 13:28",
+        },
+        {
+          name: "US$39.99",
+          description: "fe3d8216-58f9-4dc1-aaef-2f19463b5258",
+          customer: "hefovo87@ereyemind",
+          date: "7 Jul, 14:04",
+        },
+
+        {
+          name: "US$39.99",
+          description: "fe3d8216-58f9-4dc1-aaef-2f19463b5258",
+          customer: "hefovo87@ereyemind",
+          date: "7 Jul, 14:04",
+        },
+        {
+          name: "US$39.99",
+          description: "fe3d8216-58f9-4dc1-aaef-2f19463b5258",
+          customer: "hefovo87@ereyemind",
+          date: "7 Jul, 14:04",
+        },
+        {
+          name: "US$39.99",
+          description: "fe3d8216-58f9-4dc1-aaef-2f19463b5258",
+          customer: "hefovo87@ereyemind",
+          date: "7 Jul, 14:04",
+        },
+        {
+          name: "US$39.99",
+          description: "fe3d8216-58f9-4dc1-aaef-2f19463b5258",
+          customer: "hefovo87@ereyemind",
+          date: "7 Jul, 14:04",
+        },
+        {
+          name: "US$352.22",
+          description: "fe3d8216-58f9-4dc1-aaef-2f19463b5258",
+          customer: "hefovo87@ereyemind",
+          date: "7 Jul, 14:04",
+        },
+
+        {
+          name: "US$39.99",
+          description: "0d7b6ac7-dbda-4157-bae9-e64749e538f5",
+          customer: "johndoe@gmail.com",
+          date: "7 Jul, 14:01",
+        },
+        {
+          name: "US$39.99",
+          description: "eca84b25-0324-4f31-b9fb-d489048b77ae",
+          customer: "jfamosinhoaraca@iseo...",
+          date: "7 Jul, 13:28",
+        },
+        {
+          name: "US$39.99",
+          description: "fe3d8216-58f9-4dc1-aaef-2f19463b5258",
+          customer: "hefovo87@ereyemind",
+          date: "7 Jul, 14:04",
+        },
+
+        {
+          name: "US$39.99",
+          description: "fe3d8216-58f9-4dc1-aaef-2f19463b5258",
+          customer: "hefovo87@ereyemind",
+          date: "7 Jul, 14:04",
+        },
+        {
+          name: "US$39.99",
+          description: "fe3d8216-58f9-4dc1-aaef-2f19463b5258",
+          customer: "hefovo87@ereyemind",
+          date: "7 Jul, 14:04",
+        },
+        {
+          name: "US$39.99",
+          description: "fe3d8216-58f9-4dc1-aaef-2f19463b5258",
+          customer: "hefovo87@ereyemind",
+          date: "7 Jul, 14:04",
+        },
+        {
+          name: "US$339.99",
+          description: "fe3d8216-58f9-4dc1-aaef-2f19463b5258",
+          customer: "hefovo87@ereyemind",
+          date: "7 Jul, 14:04",
+        },
+      ],
+
+      items: [
+        {
+          action: "mdi-cash-multiple",
+          items: [
+            { title: "Reviews" },
+            { title: "Disputes" },
+            { title: "Top-ups" },
+            { title: "Collected fees" },
+            { title: "Transfers" },
+            { title: "Payouts" },
+            { title: "All transactions" },
+          ],
+          title: "Payments",
+        },
+      ],
+      plist: [
+        { title: "Balances", href: "/comp", icon: "mdi-swap-vertical" },
+        {
+          title: "Customers",
+          href: "/customers",
+          icon: "mdi-account-search-outline",
+        },
+        {
+          title: "Connected Accounts",
+          href: "/orders",
+          icon: "mdi-account-supervisor-circle",
+        },
+        { title: "Product", href: "/product", icon: "mdi-archive" },
+        { title: "Reports", href: "/reports", icon: "mdi-file-chart" },
+        { title: "Developers", href: "/developers", icon: "mdi-cash-multiple" },
+        {
+          title: "View test data",
+          href: "/view-test-data",
+          icon: "mdi-toggle-switch-off-outline",
+        },
+        { title: "Settings", href: "/settings", icon: "mdi-cog-outline" },
+      ],
       errors: [],
       email: "",
       emailrules: {
@@ -118,7 +301,7 @@ html {
 
 .bottom {
   width: 100%;
-  height: 500px;
+  height: 400px;
   background: linear-gradient(to right bottom, #f7fafc 50%, transparent 50%);
 }
 h1 {
@@ -167,7 +350,7 @@ h2 {
   font-weight: 600;
   font-size: 14px;
 }
-.stripe-signup{
+.stripe-signup {
   position: absolute;
   bottom: 13%;
   left: 38%;
@@ -175,7 +358,7 @@ h2 {
   font-weight: 400;
   font-size: 14px;
 }
-.stripe-footer{
+.stripe-footer {
   position: absolute;
   bottom: 9%;
   left: 38%;
@@ -198,8 +381,8 @@ a {
 a:hover {
   color: #dfd3d3;
 }
-.pur{
-color: #635cff;
+.pur {
+  color: #635cff;
 }
 
 .btn {
@@ -229,12 +412,11 @@ color: #635cff;
   align-items: flex-start;
   padding: 48px 68px 72px;
   position: absolute;
-  top: 50%;
   left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
   width: 542px;
   height: 537px;
-  left: calc(50% - 542px / 2);
-  top: calc(50% - 537px / 2 - 45px);
 
   background: #ffffff;
   box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.12),
@@ -244,7 +426,78 @@ color: #635cff;
 .v-text-field {
   width: 406px !important;
 }
-.v-text-field:hover{
-color: #B0D8F0 !important;
+.v-text-field:hover {
+  color: #b0d8f0 !important;
+}
+.box {
+  position: absolute;
+  left: 73.19%;
+  top: 32.93%;
+  bottom: 59.98%;
+  height: 40px;
+  width: 950px;
+  background: rgba(12, 0, 250, 0.5);
+
+  -webkit-transform: skewY(-5deg);
+  -moz-transform: skewY(-12deg);
+  -ms-transform: skewY(-12deg);
+  -o-transform: skewY(-12deg);
+  transform: skewY(-12deg);
+}
+
+.box > .wrapper {
+  -webkit-transform: skewY(5deg);
+  -moz-transform: skewY(5deg);
+  -ms-transform: skewY(5deg);
+  -o-transform: skewY(5deg);
+  transform: skewY(5deg);
+}
+.box1 {
+  position: absolute;
+  right: 78.19%;
+  left: -0.03%;
+  bottom: 32.33%;
+  top: 90.78%;
+  height: 40px;
+  width: 560px;
+  background: rgba(12, 0, 250, 0.5);
+
+  -webkit-transform: skewY(-5deg);
+  -moz-transform: skewY(-11.5deg);
+  -ms-transform: skewY(-11.5deg);
+  -o-transform: skewY(-11.5deg);
+  transform: skewY(-11.5deg);
+}
+
+.box1 > .wrapper1 {
+  -webkit-transform: skewY(5deg);
+  -moz-transform: skewY(5deg);
+  -ms-transform: skewY(5deg);
+  -o-transform: skewY(5deg);
+  transform: skewY(5deg);
+}
+.box2 {
+  position: absolute;
+  left: 1.46%;
+  right: 87.5%;
+  top: 89.82%;
+  bottom: 32.61%;
+  height: 50px;
+  opacity: 0.5;
+  background: #81e9ff;
+
+  -webkit-transform: skewY(-5deg);
+  -moz-transform: skewY(-12deg);
+  -ms-transform: skewY(-12deg);
+  -o-transform: skewY(-12deg);
+  transform: skewY(-12deg);
+}
+
+.box2 > .wrapper1 {
+  -webkit-transform: skewY(5deg);
+  -moz-transform: skewY(5deg);
+  -ms-transform: skewY(5deg);
+  -o-transform: skewY(5deg);
+  transform: skewY(5deg);
 }
 </style>
